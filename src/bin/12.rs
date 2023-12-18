fe2c9f7d42de2e427757144554dd4e6bafa2aae4
Sammy Humphreys <Sammyh2009@outlook.com>
2023-12-18 16:39:16 +0000

    solve day 12 part 1

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -37,41 +37,83 @@ fn get_possible_arangements(springs: Vec<char>) -> Vec<Vec<bool>> {
     possible
 }
 
-fn fits_critera(springs: &Vec<bool>, groups: Vec<u32>) -> bool {
+fn fits_critera(springs: &Vec<bool>, groups: &Vec<u32>) -> bool {
     let mut current_group_len = 0;
     let mut current_group_index = 0;
+    let mut had_last_group = false;
 
     for spring in springs {
         if *spring == false {
-            if groups[current_group_index] != current_group_len {
-                return false;
-            }
-            current_group_len = 0;
-            current_group_index += 1;
+            if current_group_len > 0 {
+                if current_group_index > groups.len() || groups.get(current_group_index) != Some(&current_group_len) {
+                    return false;
+                }
+                
+                if current_group_index == groups.len() - 1 {
+                    had_last_group = true;
+                }
 
-            if current_group_index >= groups.len() {
-                return false;
+                current_group_len = 0;
+                current_group_index += 1;
             }
         }
         else {
+            if had_last_group {
+                return false;
+            }
+
             current_group_len += 1;
         }
     }
 
-    if groups[current_group_index] != current_group_len {
-        return false;
+    if current_group_index == groups.len() {
+        return true;
+    }
+    else if current_group_index == groups.len() - 1 && groups[current_group_index] == current_group_len {
+        return true;
     }
     else {
-        return true;
+        return false;
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<usize> {
     let input = parse(input);
-    dbg!(get_possible_arangements("#.#.###".chars().collect()).iter().map(|springs| fits_critera(dbg!(springs), vec![1, 1, 3])).filter(|result| *result).collect_vec());
-    dbg!(fits_critera(&vec![true, false, true, false, true, true, true], vec![1, 1, 3]));
+    let mut total = 0;
+
+    for (springs, groups) in input.iter() {
+        total += get_possible_arangements(springs.chars().collect()).iter()
+            .map(|springs| fits_critera(springs, groups))
+            .filter(|result| *result).count();
+    }
+
+    //dbg!(get_possible_arangements("..#...#...###.".chars().collect()));
+
+    // dbg!(fits_critera(&vec![ false,
+    //     true,
+    //     true,
+    //     true,
+    //     false,
+    //     false,
+    //     true,
+    //     true,
+    //     false,
+    //     true,
+    //     false,
+    //     true,], &vec![3, 2, 1]));
     //dbg!(get_possible_arangements("?.?".chars().collect()));
-    None
+    // let arrangements = get_possible_arangements("?###????????".chars().collect());
+    // let results = arrangements.clone().iter()
+    //     .map(|springs| fits_critera(springs, &vec![3, 2, 1]))
+    //     .collect::<Vec<_>>();
+
+    // for i in 0..arrangements.len() {
+    //     if results[i] == true {
+    //         dbg!(&arrangements[i]);
+    //     }
+    // }
+
+    Some(total)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -79,8 +121,16 @@ pub fn part_two(input: &str) -> Option<u32> {
     None
 }
 
-fn parse(input: &str) -> Vec<&str> {
-    input.split("\n").collect()
+fn parse(input: &str) -> Vec<(&str, Vec<u32>)> {
+    let input: Vec<_> = input.split("\n").collect();
+    let mut springs: Vec<(&str, Vec<u32>)> = vec![];
+
+    for set in input {
+        let mut set = set.split(" ");
+        springs.push((set.next().unwrap(), set.next().unwrap().split(",").map(|group| group.parse().unwrap()).collect()));
+    }
+
+    springs
 }
 
 #[cfg(test)]
